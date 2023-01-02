f6dbbecdd4564729bb65dee060a97dd51f2f137c
BrandPM18 <bpalaciosm@uni.pe>
2023-01-01 23:17:57 -0500

    Created all implement Tables

--- assets/drawSQL-pgsql-export-2022-10-28.sql
+++ assets/drawSQL-pgsql-export-2022-10-28.sql
@@ -1,49 +1,65 @@
-CREATE TABLE "Trip"(
-    "ID" BIGINT NOT NULL,
-    "taxi_ID" BIGINT NOT NULL,
-    "start_Timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "end_Timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "seconds" INTEGER NOT NULL,
-    "miles" DOUBLE PRECISION NOT NULL,
-    "fare" DOUBLE PRECISION NOT NULL,
-    "tips" DOUBLE PRECISION NULL,
-    "tolls" DOUBLE PRECISION NULL,
-    "extras" DOUBLE PRECISION NULL,
-    "total_fare" DOUBLE PRECISION NOT NULL
+create table public."CommunityArea"
+(
+    id       bigserial
+        primary key,
+    name     text                         not null,
+    area     double precision             not null,
+    len      double precision             not null,
+    geometry geometry(MultiPolygon, 4326) not null
 );
-ALTER TABLE
-    "Trip" ADD PRIMARY KEY("ID");
-ALTER TABLE
-    "Trip" ADD CONSTRAINT "trip_taxi_id_unique" UNIQUE("taxi_ID");
-CREATE TABLE "Points"(
-    "id_trip" INTEGER NOT NULL,
-    "community_area" BIGINT NOT NULL,
-    "location" TEXT NOT NULL,
-    "geometry" geography(POINT, 4326) NOT NULL,
-    "isPick" BOOLEAN NOT NULL
+
+create index "idx_CommunityArea_geometry"
+    on public."CommunityArea" using gist (geometry);
+
+
+create table public."Point"
+(
+    id_trip           text                  not null
+        constraint points_id_trip_foreign
+            references public."Trip",
+    id_community_area bigint                not null
+        constraint points_community_area_foreign
+            references public."CommunityArea",
+    location          text                  not null,
+    geometry          geometry(Point, 4326) not null,
+    "isPick"          boolean               not null
 );
-ALTER TABLE
-    "Points" ADD CONSTRAINT "points_id_trip_unique" UNIQUE("id_trip");
-ALTER TABLE
-    "Points" ADD CONSTRAINT "points_community_area_unique" UNIQUE("community_area");
-CREATE TABLE "Community Area"(
-    "id" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "area" DOUBLE PRECISION NOT NULL,
-    "len" DOUBLE PRECISION NOT NULL,
-    "geometry" geography(MULTIPOLYGON, 4326) NOT NULL
+
+alter table public."Point"
+    owner to admin;
+
+create index "idx_Point_geometry"
+    on public."Point" using gist (geometry);
+
+create table public."Taxi"
+(
+    id      text not null
+        primary key,
+    company text
 );
-ALTER TABLE
-    "Community Area" ADD PRIMARY KEY("id");
-CREATE TABLE "Taxi"(
-    "id" INTEGER NOT NULL,
-    "company" TEXT NOT NULL
+
+alter table public."Taxi"
+    owner to admin;
+
+create table public."Trip"
+(
+    -- text can not be auto increment
+    id           serial
+        primary key,
+    id_taxi      text             not null
+        constraint trip_taxi_id_foreign
+            references public."Taxi",
+    "createAt"   timestamp        not null,
+    "finishedAt" timestamp        not null,
+    seconds      integer          not null,
+    miles        double precision not null,
+    fare         double precision not null,
+    tips         double precision not null,
+    tolls        double precision not null,
+    extras       double precision not null,
+    total_fare   double precision not null
 );
-ALTER TABLE
-    "Taxi" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "Trip" ADD CONSTRAINT "trip_taxi_id_foreign" FOREIGN KEY("taxi_ID") REFERENCES "Taxi"("id");
-ALTER TABLE
-    "Points" ADD CONSTRAINT "points_id_trip_foreign" FOREIGN KEY("id_trip") REFERENCES "Trip"("ID");
-ALTER TABLE
-    "Points" ADD CONSTRAINT "points_community_area_foreign" FOREIGN KEY("community_area") REFERENCES "Community Area"("id");
+
+alter table public."Trip"
+    owner to admin;
+
